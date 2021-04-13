0cdf4b20e4fbe6b384eeb083efbea41b88c1df13
demon <ps3psn3@mail.ru>
2021-04-13 13:39:34 +0300

    gg

--- dz.py
+++ dz.py
@@ -1,7 +1,22 @@
-weight  =  int(input("Введите вес "))             
-height  =  int(input("Введите рост "))
-bmi  =  int(weight / height**2 * 10000)
-print("Ваш индекс массы тела: ", bmi)
-x  =  list("================================================") 
-x.insert(bmi, "||")
-print("20", x[20:50], "50")
+weight = int(input(f'Введите вес '))
+height = int(input(f'Введите рост '))
+gender = str(input('Введите пол (м\ж) '))
+bmi = int(weight / height**2 * 10000)
+print(f'Ваш индекс массы тела: {bmi}')
+x = list("================================================")
+x.insert(bmi, "|")
+print(''.join((str(i) for i in x[20:50])))
+if gender == ('м'):
+    if bmi < 20:
+        print('Поднабрать бы')
+    elif bmi < 30:
+        print('нормас')
+    elif bmi > 30:
+        print('харош)')
+if gender == ('ж'):
+    if bmi < 20:
+        print('улет')
+    elif bmi < 30:
+        print('нормас')
+    elif bmi > 30:
+        print('крошка)')
